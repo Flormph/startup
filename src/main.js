c22b1f15b45cfb1e34580e19607a75f441c9aa6b
Joseph <josephlee11111010000@gmail.com>
2026-07-07 21:06:49 -0700

    Adds script for hamburger menu in nav-bar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import '../main.css'
+
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('hidden');
+});
