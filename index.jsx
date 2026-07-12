caa76dc6d36f869d471205e454b3b9c431c4fab0
Joseph <josephlee11111010000@gmail.com>
2026-07-11 19:06:22 -0600

    Adds Browser Router tags so that way the hide nav on page switch functionality works

--- index.jsx
+++ index.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './src/app.css'
 import App from './src/app.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
