d016fa19d91b1b0d209a7a2c0475806756e2120e
Joseph <josephlee11111010000@gmail.com>
2026-07-12 03:03:14 -0600

    Updates about section

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 export function About() {
     return (
-        <main>
-            <p>
-                Gedidone is a web application that allows users to create and manage sticky notes, as well as interact with a virtual pet meadow. The application is designed to be user-friendly and accessible, providing a seamless experience for users to organize their thoughts and enjoy a virtual pet environment.
+        <main className="flex flex-col items-center text-center m-4">
+            <p className="p-4">
+                Gedidone is a web application that allows users to create and manage task reminder sticky notes, as well as interact with a virtual pet. The pets health and happiness are dependent on the users consitent follow through of sticky note tasks.
             </p>
 
-            <p>
+            <p className="p-4">
                 The idea came from a desire to create a low-friction life management tool targeted towards individuals with attention disorders.
+            </p>
+
+            <p className="p-4">
+                The game was because I was bored.
             </p>
         </main>
     );
