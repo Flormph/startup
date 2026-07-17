59c5574b239f05f76cd4b771dc53e220a275f845
Joseph <josephlee11111010000@gmail.com>
2026-07-17 11:32:19 -0700

    Added comments

--- src/game/collision.js
+++ src/game/collision.js
@@ -1,8 +1,8 @@
 export function checkCollision(rectA, rectB) {
     return (
-        rectA.x < rectB.x + rectB.width &&
-        rectA.x + rectA.width > rectB.x &&
-        rectA.y < rectB.y + rectB.height &&
-        rectA.y + rectA.height > rectB.y
+        rectA.x < rectB.x + rectB.width && // rectA to the left of rectB's right side
+        rectA.x + rectA.width > rectB.x && // rectA to the right of rectB's left side
+        rectA.y < rectB.y + rectB.height && // rectA above rectB's bottom side
+        rectA.y + rectA.height > rectB.y // rectA below rectB's top side
     );
 }
