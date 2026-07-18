2ed83756029c440f0be610a54429bb24a4d6ee18
Joseph <josephlee11111010000@gmail.com>
2026-07-18 13:02:39 -0700

    Adds palette

--- Tools/AxolotlMapEditor/Form1.cs
+++ Tools/AxolotlMapEditor/Form1.cs
@@ -8,6 +8,7 @@ public partial class Form1 : Form
     private const int GridWidth = 30;
     private const int GridHeight = 17;
     private const int CellSize = 20;
+    private const int PaletteHeight = 40;
 
     private RoomData currentRoom;
     private TileType selectedTileType = TileType.Wall;
@@ -20,11 +21,58 @@ public partial class Form1 : Form
         this.ClientSize = new Size(GridWidth * CellSize + 20, GridHeight * CellSize + 20);
         this.DoubleBuffered = true; // Reduce flickering
 
+        BuildPalette();
+
         this.Paint += Form1_Paint;
         this.MouseClick += Form1_MouseClick;
     }
 
-    private void Form1_Paint(object sender, PaintEventArgs e)
+    private void BuildPalette()
+    {
+        var tileTypes = new[]
+        {
+            TileType.Empty,
+            TileType.Wall,
+            TileType.ExitLeft,
+            TileType.ExitRight,
+            TileType.ExitUp,
+            TileType.ExitDown,
+            TileType.ExitOverride,
+            TileType.Teleport,
+            TileType.PlayerSpawn,
+            TileType.EnemySpawn,
+            TileType.ItemSpawn
+        };
+
+        int buttonWidth = 70;
+        int x = 10;
+
+        foreach (var type in tileTypes)
+        {
+            var button = new Button
+            {
+                Text = type.ToString(),
+                Location = new Point(x, GridHeight * CellSize + 10),
+                Size = new Size(buttonWidth, PaletteHeight - 10),
+                Tag = type // Store the tile type in the button's Tag property
+            };
+            button.Click += PaletteButton_Click;
+            this.Controls.Add(button);
+
+            x += buttonWidth + 5;
+        }
+    }
+
+    private void PaletteButton_Click(object? sender, EventArgs e)
+    {
+        var button = (Button)sender!;
+        if (button.Tag is TileType type)
+        {
+            selectedTileType = type;
+        }
+    }
+
+    private void Form1_Paint(object? sender, PaintEventArgs e)
     {
         var g = e.Graphics;
 
@@ -61,15 +109,14 @@ public partial class Form1 : Form
         }
     }
 
-    private void Form1_MouseClick(object sender, MouseEventArgs e)
+    private void Form1_MouseClick(object? sender, MouseEventArgs e)
     {
         int x = e.X / CellSize;
         int y = e.Y / CellSize;
 
-        if (x >= 0 && x < GridWidth && y >= 0 && y < GridHeight)
-        {
-            currentRoom.Tiles[y][x] = new Tile(selectedTileType);
-            this.Invalidate(); // Redraw the form
-        }
+        if (x < 0 || x >= GridWidth || y < 0 || y >= GridHeight) return;
+
+        currentRoom.Tiles[y][x] = new Tile(selectedTileType);
+        this.Invalidate(); // Redraw the form
     }
 }
