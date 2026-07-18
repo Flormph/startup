78c3d2d0854429e7edc1518f292fbc8c33a346a5
Joseph <josephlee11111010000@gmail.com>
2026-07-18 12:54:39 -0700

    Sets up initial map editor tools (RoomData object, color switch for tile types, responsive gui, mouseclick interaction)

--- Tools/AxolotlMapEditor/Form1.cs
+++ Tools/AxolotlMapEditor/Form1.cs
@@ -1,9 +1,75 @@
 namespace AxolotlMapEditor;
 
+using System.Drawing;
+using System.Windows.Forms;
+
 public partial class Form1 : Form
 {
+    private const int GridWidth = 30;
+    private const int GridHeight = 17;
+    private const int CellSize = 20;
+
+    private RoomData currentRoom;
+    private TileType selectedTileType = TileType.Wall;
+
     public Form1()
     {
-        InitializeComponent();
+        currentRoom = new RoomData(GridWidth, GridHeight);
+
+        this.Text = "Axolotl Map Editor";
+        this.ClientSize = new Size(GridWidth * CellSize + 20, GridHeight * CellSize + 20);
+        this.DoubleBuffered = true; // Reduce flickering
+
+        this.Paint += Form1_Paint;
+        this.MouseClick += Form1_MouseClick;
+    }
+
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+        var g = e.Graphics;
+
+        // Draw grid
+        for (int x = 0; x < GridWidth; x++)
+        {
+            for (int y = 0; y < GridHeight; y++)
+            {
+                var tile = currentRoom.Tiles[y][x];
+                var color = GetColorForTile(tile.Type);
+
+                var rect = new Rectangle(x * CellSize, y * CellSize, CellSize, CellSize);
+                g.FillRectangle(new SolidBrush(color), rect);
+                g.DrawRectangle(Pens.Black, rect);
+            }
+        }
+    }
+
+    private Color GetColorForTile(TileType type)
+    {
+        switch (type)
+        {
+            case TileType.Empty: return Color.White;
+            case TileType.Wall: return Color.SaddleBrown;
+            case TileType.ExitLeft:
+            case TileType.ExitRight:
+            case TileType.ExitUp:
+            case TileType.ExitDown: return Color.Gold;
+            case TileType.Teleport: return Color.Purple;
+            case TileType.EnemySpawn: return Color.Red;
+            case TileType.ItemSpawn: return Color.LimeGreen;
+            case TileType.PlayerSpawn: return Color.DodgerBlue;
+            default: return Color.LightGray;
+        }
+    }
+
+    private void Form1_MouseClick(object sender, MouseEventArgs e)
+    {
+        int x = e.X / CellSize;
+        int y = e.Y / CellSize;
+
+        if (x >= 0 && x < GridWidth && y >= 0 && y < GridHeight)
+        {
+            currentRoom.Tiles[y][x] = new Tile(selectedTileType);
+            this.Invalidate(); // Redraw the form
+        }
     }
 }
