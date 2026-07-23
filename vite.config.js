b995aadaf3a67f50a81977651a238b6d1cf646e1
Joseph <josephlee11111010000@gmail.com>
2026-07-22 23:51:27 -0600

    Changes port target to 4000

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000', // wherever your Express server is actually running
+        changeOrigin: true,
+      },
+    },
+  },
 })
 
