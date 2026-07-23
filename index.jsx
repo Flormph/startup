a004e83c34463ddb4bff2af975cef2907dc0b24f
Joseph <josephlee11111010000@gmail.com>
2026-07-23 12:28:28 -0600

    Fixes an issue where nav bar would appear but be unnaccesible if logged out

--- index.jsx
+++ index.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './src/app.css'
 import App from './src/app.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './auth/auth.jsx';
-import { RouteGuard } from './route-guard/route-guard.jsx';
+import { AuthProvider } from './src/auth/auth.jsx';
+import { RouteGuard } from './src/route-guard/route-guard.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
