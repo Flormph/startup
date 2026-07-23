c09eb51c2262aa313eb15cba29ade29e3f2644fa
Joseph <josephlee11111010000@gmail.com>
2026-07-23 12:23:13 -0600

    Moves AuthProvider to this file

--- index.jsx
+++ index.jsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './src/app.css'
 import App from './src/app.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './auth/auth.jsx';
+import { RouteGuard } from './route-guard/route-guard.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <App />
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </BrowserRouter>
 )
