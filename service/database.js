12f8578e471234fc6cf9288d3649157e3275d987
Joseph <josephlee11111010000@gmail.com>
2026-07-29 22:19:25 -0600

    Adds mongodb to dependencies

--- service/database.js
+++ service/database.js
@@ -2,7 +2,7 @@
 //dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const { MongoClient } = require('mongodb');
-const config = require('../Mongo/dbConfig.json');
+const config = require('./dbConfig.json');
 
 const url =
     `mongodb+srv://${config.userName}:${config.password}@${config.hostname}/?appName=Gedidone`;
